aeffeeba7b4da5609cfa6aa874f9360659ab5533
YANG Huan <sy.yanghuan@gmail.com>
2017-03-24 15:43:55 +0800

    update

--- external/bridgelua-editor/csharp-codes/Hello-World.cs
+++ external/bridgelua-editor/csharp-codes/Hello-World.cs
@@ -3,7 +3,7 @@ using System;
 namespace HelloBridgeLua {
     public static class Program {
         public static void Main() {
-            Console.WriteLine("Hello Bridge.Lua");
+            Console.WriteLine("hello lua!");
         }
     }
 }
