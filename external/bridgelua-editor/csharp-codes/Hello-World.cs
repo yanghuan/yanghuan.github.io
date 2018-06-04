b3beb042b6e2ce676814b90065d1c470d1d496ee
yanghuan <sy.yanghuan@gmail.com>
2018-06-04 13:33:12 +0800

    format

--- external/bridgelua-editor/csharp-codes/Hello-World.cs
+++ external/bridgelua-editor/csharp-codes/Hello-World.cs
@@ -1,9 +1,9 @@
 using System;
 
 namespace HelloBridgeLua {
-    public static class Program {
-        public static void Main() {
-            Console.WriteLine("Hello Bridge.Lua");
-        }
+  public static class Program {
+    public static void Main() {
+      Console.WriteLine("Hello Bridge.Lua");
     }
+  }
 }
--- external/bridgelua-editor/csharp-codes/Linq.cs
+++ external/bridgelua-editor/csharp-codes/Linq.cs
@@ -2,18 +2,18 @@ using System;
 using System.Linq;
 
 namespace Linq {
-    public static class Program {
-        public static void Main() {
-            string[] names = new string[] { "Yang", "Zhang", "Li", "Wang", "Mao" };
+  public static class Program {
+    public static void Main() {
+      string[] names = new string[] { "Yang", "Zhang", "Li", "Wang", "Mao" };
 
-            var query =
-                from n in names
-                where n.Contains("a")
-                orderby n
-                select n;
+      var query =
+          from n in names
+          where n.Contains("a")
+          orderby n
+          select n;
 
-            string result = query.Aggregate((x, y) => x + ':' + y);
-            Console.WriteLine(result);
-        }
+      string result = query.Aggregate((x, y) => x + ':' + y);
+      Console.WriteLine(result);
     }
+  }
 }
