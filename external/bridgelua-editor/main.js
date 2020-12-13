7c0eb87cf819d6a7ea0c66161e02b7420ff7e1c8
YANG Huan <sy.yanghuan@gmail.com>
2020-12-13 21:55:40 +0800

    run csharpluacompiler at Blazor

--- external/bridgelua-editor/main.js
+++ external/bridgelua-editor/main.js
@@ -1,73 +1,73 @@
 $(function () {
-    var samples = [
-        "Hello World",
-        "Fibonacci",
-        "TimeoutQueue",
-        "Linq"
-    ];
+  var samples = [
+    "Hello World",
+    "Fibonacci",
+    "TimeoutQueue",
+    "Linq"
+  ];
 
-    $.each(samples, function (k, v) {
-        var path = "./csharp-codes/" + v.replace(" ", "-") + ".cs"
-        $.get(path, function (data) {
-            var option = $("<option>").text("Sample: " + v).val(data);
-            $("#examples").append(option);
-        }, "text");
+  $.each(samples, function (k, v) {
+    var path = "./csharp-codes/" + v.replace(" ", "-") + ".cs.txt"
+    $.get(path, function (data) {
+      var option = $("<option>").text("Sample: " + v).val(data);
+      $("#examples").append(option);
+    }, "text");
+  });
+
+  require.config({
+    paths: {
+      'vs': './monaco-editor/min/vs'
+    }
+  });
+  require(['vs/editor/editor.main'], start);
+
+  function start() {
+    jQuery.support.cors = true;
+    var sampleSelect = $("#examples");
+    var loading = $("#loading");
+    var autoRunTimer = null;
+
+    var csharpEditor = monaco.editor.create(document.getElementById('CSharpEditor'), {
+      value: "",
+      language: 'csharp'
+    });
+    csharpEditor.onKeyUp(function () {
+      if (autoRunTimer) {
+        clearTimeout(autoRunTimer);
+        autoRunTimer = null;
+      }
+      autoRunTimer = setTimeout(run, 2000);
     });
 
-    require.config({
-        paths: {
-            'vs': './monaco-editor/min/vs'
-        }
+    var luaEditor = monaco.editor.create(document.getElementById('LuaEditor'), {
+      value: "",
+      language: 'lua'
     });
-    require(['vs/editor/editor.main'], start);
 
-    function start() {
-        jQuery.support.cors = true;
-        var sampleSelect = $("#examples");
-        var loading = $("#loading");
-        var autoRunTimer = null;
-
-        var csharpEditor = monaco.editor.create(document.getElementById('CSharpEditor'), {
-            value: "",
-            language: 'csharp'
-        });
-        csharpEditor.onKeyUp(function () {
-            if(autoRunTimer) {
-                clearTimeout(autoRunTimer);
-                autoRunTimer = null;        
-            }
-            autoRunTimer = setTimeout(run, 2000);
-        });
-
-        var luaEditor = monaco.editor.create(document.getElementById('LuaEditor'), {
-            value: "",
-            language: 'lua'
-        });
-
-        function switchElement() { 
-            loading.toggle();
-        };
-
-        function run() {
-            switchElement();
-            $.post("https://csharpluacompiler.appspot.com/api/Compiler", csharpEditor.getValue(), function (data) {
-                luaEditor.setValue(data);
-                switchElement();
-            }).error(function (response) {
-                console.log(response);
-                luaEditor.setValue("An unexpected error ocurred... please try again later or use proxy.");
-                switchElement();
-            });
-        };
-
-        function setCurSelect() {
-            csharpEditor.setValue(sampleSelect.val());
-            run();
-        };
-
-        setCurSelect();
-        sampleSelect.change(function () {
-            setCurSelect();
-        });
+    function switchElement() {
+      loading.toggle();
     };
+
+    function run(loading) {
+      if (loading) {
+        switchElement();
+      }
+      DotNet.invokeMethodAsync('CSharpLuaBlazor', 'Compile', csharpEditor.getValue()).then(data => {
+        luaEditor.setValue(data);
+        switchElement();
+      });
+    };
+
+    function setCurSelect(loading) {
+      csharpEditor.setValue(sampleSelect.val());
+      run(loading);
+    };
+
+    sampleSelect.change(function () {
+      setCurSelect(true);
+    });
+
+    switchElement();
+    window.start = () => setCurSelect(false);
+  };
 });
